10c194f7acb1e468bfc6e176538d29f00065c2db
Marcel <marcel.th1@gmx.de>
2026-03-25 13:26:43 +0100

    Vorbereitung für den nächsten Push

--- 02_Software/New_Image_Capture/Secure_nsclib/FSBL/USBPD/App/usbpd_dpm_conf.h
+++
@@ -1,2 +0,0 @@
-// This file is a copy of the Target configuration for USBPD
-#include "../Target/usbpd_dpm_conf.h"
--- 02_Software/Thesis_Robot_Pose/build/Debug/CMakeFiles/clean_additional.cmake
+++
@@ -1,9 +0,0 @@
-# Additional clean files
-cmake_minimum_required(VERSION 3.16)
-
-if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "Debug")
-  file(REMOVE_RECURSE
-  "C:\\Dev\\Thesis_Robot_Pose\\Appli\\build"
-  "C:\\Dev\\Thesis_Robot_Pose\\FSBL\\build"
-  )
-endif()
